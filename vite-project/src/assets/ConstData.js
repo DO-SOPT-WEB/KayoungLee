06ed7f519e10c4565509035794a0d170ca94a1f2
imeureka <lky062@naver.com>
2023-11-07 17:41:13 +0900

    feat : const Data 추가

---
+++ vite-project/src/assets/ConstData.js
@@ -0,0 +1,117 @@
+import 강동원 from "./img/sopt강동원.png";
+import 강다니엘 from "./img/sopt강다니엘.png";
+import 강하늘 from "./img/sopt강하늘.png";
+import 공유 from "./img/sopt공유.png";
+import 권상우 from "./img/sopt권상우.png";
+import 김대명 from "./img/sopt김대명.png";
+import 김우빈 from "./img/sopt김우빈.png";
+import 김주혁 from "./img/sopt김주혁.png";
+import 박형식 from "./img/sopt박형식.png";
+import 성준 from "./img/sopt성준.png";
+import 송승헌 from "./img/sopt송승헌.png";
+import 우도환 from "./img/sopt우도환.png";
+import 이민기 from "./img/sopt이민기.png";
+import 이상윤 from "./img/sopt이상윤.png";
+import 임시완 from "./img/sopt임시완.png";
+
+export const DATA = [
+  // 1. 소년, 청년, 중년
+  // 2. 착한, 나쁜, 마성
+  // 3. 유쌍, 무쌍
+  {
+    category: "중년",
+    type: "마성",
+    eye: true,
+    src: 강동원,
+  },
+  {
+    category: "소년",
+    type: "착한",
+    eye: false,
+    src: 강다니엘,
+  },
+  {
+    category: "청년",
+    type: "착한",
+    eye: false,
+    src: 강하늘,
+  },
+  {
+    category: "중년",
+    type: "마성",
+    eye: false,
+    src: 공유,
+  },
+  {
+    category: "중년",
+    type: "나쁜",
+    eye: true,
+    src: 권상우,
+  },
+  {
+    category: "청년",
+    type: "착한",
+    eye: false,
+    src: 김대명,
+  },
+  {
+    category: "청년",
+    type: "나쁜",
+    eye: false,
+    src: 김우빈,
+  },
+  {
+    category: "중년",
+    type: "착한",
+    eye: true,
+    src: 김주혁,
+  },
+  {
+    category: "소년",
+    type: "나쁜",
+    eye: true,
+    src: 박형식,
+  },
+  {
+    category: "소년",
+    type: "착한",
+    eye: false,
+    src: 강다니엘,
+  },
+  {
+    category: "청년",
+    type: "마성",
+    eye: true,
+    src: 성준,
+  },
+  {
+    category: "중년",
+    type: "착한",
+    eye: true,
+    src: 송승헌,
+  },
+  {
+    category: "소년",
+    type: "나쁜",
+    eye: true,
+    src: 우도환,
+  },
+  {
+    category: "청년",
+    type: "착한",
+    eye: false,
+    src: 이민기,
+  },
+  {
+    category: "중년",
+    type: "나쁜",
+    eye: false,
+    src: 이상윤,
+  },
+  {
+    category: "청년",
+    type: "착한",
+    eye: true,
+    src: 임시완,
+  },
+];
