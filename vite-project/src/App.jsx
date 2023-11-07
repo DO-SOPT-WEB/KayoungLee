8e186635730ddf9248e4358390a1627298cc834a
imeureka <lky062@naver.com>
2023-11-08 03:12:57 +0900

    design(main) : wrapper 수정

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,13 @@
-import styled from "styled-components";
 import Header from "../components/Header";
 import MainWrapper from "../components/MainWrapper";
 
 function App() {
   return (
-    <Wrapper>
+    <>
       <Header />
       <MainWrapper />
-    </Wrapper>
+    </>
   );
 }
 
 export default App;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 5rem;
-`;
