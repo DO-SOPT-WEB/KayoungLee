30c7cf148cf227e33ad75fb2dd95055c9a65b208
imeureka <lky062@naver.com>
2023-11-08 02:20:58 +0900

    design : main 헤더 스타일 구현 및 글로벌 스토일 지정

---
+++ vite-project/style/Header.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  display: flex;
+  width: 100%;
+  height: 10vh;
+  justify-content: center;
+  align-items: center;
+  background-color: #72bdff;
+`;
+
+export const HeaderTitle = styled.h1``;
---
+++ vite-project/style/MainStyle.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  display: flex;
+  width: 80vw;
+  height: 80vh;
+
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 6rem;
+  background-color: #72bdff;
+`;
+
+export const BtnWrapper = styled.article`
+  display: flex;
+  top: -23px;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  gap: 5rem;
+`;
