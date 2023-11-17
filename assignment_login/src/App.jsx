29d7bddcddeae74f2466cf5af3d61d93f0a4a287
imeureka <lky062@naver.com>
2023-11-17 20:58:51 +0900

    Feat[main] : RouterProvider 이용해서 라우팅

--- assignment_login/src/App.jsx
+++ assignment_login/src/App.jsx
@@ -1,7 +1,48 @@
-import "./App.css";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+
+const DashBoard = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashBoard />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+
+  {
+    path: "/Signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return <>hello this is login</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
